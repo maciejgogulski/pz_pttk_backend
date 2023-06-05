ee39adaf5657445a1107aa985615278feccc7c5a
edipl-projects <116559286+edipl-projects@users.noreply.github.com>
2023-06-05 13:35:02 +0200

    #PP-89 update

--- database/migrations/2023_05_27_154130_create_roles_table.php
+++ database/migrations/2023_05_27_154130_create_roles_table.php
@@ -14,13 +14,18 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            /*
             $table->boolean('tatra_podtatrze')->default(false);
             $table->boolean('tatra_slowackie')->default(false);
             $table->boolean('beskidy_zachodnie')->default(false);
             $table->boolean('beskidy_wschodnie')->default(false);
             $table->boolean('gory_swietokrzyskie')->default(false);
             $table->boolean('sudety')->default(false);
-            $table->boolean('słowacja')->default(false);
+            $table->boolean('słowacja')->default(false);*/
+            
+            $table->unsignedBigInteger('mountain_group_id')->nullable();
+            $table->foreign('mountain_group_id')->references('id')->on('mountain_groups')->onDelete('set null');
+            $table->boolean('posiada_uprawnienia')->default(false);
             $table->timestamps();
         });
     }
@@ -30,6 +35,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropForeign(['mountain_group_id']);
+        });
+
         Schema::dropIfExists('roles');
     }
 };
--- database/migrations/2023_05_27_154430_create_users_roles_table.php
+++ database/migrations/2023_05_27_154430_create_users_roles_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -14,6 +15,7 @@ return new class extends Migration
         Schema::create('user_roles', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained();
             $table->foreignId('role_id')->constrained();
+            $table->date('data_nadania_uprawnienia')->default(DB::raw('CURRENT_DATE'));
             $table->timestamps();
         });
     }
